5a0f625c040e8024151103da467694e61f2b4a2a
Kshitiz Tomar <kshitiz.tomar@skedgroup.in>
2025-11-10 18:00:54 +0530

    Final Navbar commit

--- BookMyTable/src/main.jsx
+++ BookMyTable/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* ✅ Wrap the whole App inside BrowserRouter */}
+   
     <BrowserRouter>
       <App />
     </BrowserRouter>
